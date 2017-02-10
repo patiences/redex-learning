f18a9d79c52e7279f755a1bc2cfd8ca4a1ff9a96
Patience Shyu <patienceshyu@gmail.com>
2017-02-10 08:29:15 -0800

    let

---
+++ long-tutorial/let.rkt
@@ -0,0 +1,27 @@
+#lang racket
+(require redex)
+(require "developing-a-language.rkt")
+(require "lookup.rkt")
+
+;; DON'T DO THIS
+;; A LET IS JUST SYNTACTIC SUGAR FOR A LAMBDA WOOOOOOOOO 
+#; 
+(define-metafunction Env
+  let : env e -> any
+  [(let ((x_0 e_0) ...) x) ; e is an x 
+   (lookup x ((x_0 e0) ...))]
+  [(let ((x_0 e_0) ...) (lambda (x ...) e_body)) ; e is a lambda 
+   ...]
+
+  [(let ((x_0 e_0) ...) (e_1 e_2 ...))
+   (e_1_go e_2_go)
+   (where e_1_go (let ((x_0 e_0) ...) e_1))
+   (where e_2_go (let ((x_0 e_0) ...)) (e_2 ...))] ; e is a list of exps 
+
+  [(let ((x_0 e_0) ...) natural) natural]) ; e is a natural
+
+
+(define-metafunction Lambda-calculus
+  let : ((x any) ...) any -> any
+  [(let ([x_lhs any_rhs] ...) any_body)
+   ((lambda (x_lhs ...) any_body) any_rhs ...)])
